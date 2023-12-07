ddc91268b7019e9bb1cfc54504f25ba4c17c4334
Spencer Lewis <spence.914@gmail.com>
2023-12-07 13:18:54 -0800

    create loadTweets function that sends an ajax get request to /tweets/ and passes the response to renderTweets, so that the tweets displayed on the page are dynamically rendered from the info at /tweets/

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -35,31 +35,31 @@
 // };
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const renderTweets = function (tweets) {
   // loops through tweets
@@ -104,5 +104,15 @@ const createTweetElement = function (tweet) {
 
 $(document).ready(function () {
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.get("/tweets/", function(response) {
+      renderTweets(response);
+    });
+  };
+
+  loadTweets();
+
+
+
+  //renderTweets(data);
 });
