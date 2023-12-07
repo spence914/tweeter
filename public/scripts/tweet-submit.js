d938e6ddc87cf42bbee18d46d62c3cd7a30ec412
Spencer Lewis <spence.914@gmail.com>
2023-12-07 13:34:03 -0800

    modify tweet-submit.js script to send alert messages and not submit form if tweet has 0 characters or more than 140 characters

--- public/scripts/tweet-submit.js
+++ public/scripts/tweet-submit.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
 
-  $("main section form").on("submit", function(event) {
+  $("main section form").on("submit", function (event) {
     event.preventDefault();
 
     const tweetText = $(this).serialize();
 
-    console.log(tweetText);
+
+    if (tweetText.length === 5) {
+      return alert("Tweets cannot be empty");
+    } else if (tweetText.length > 145) {
+      return alert("Tweets cannot be longer than 140 characters");
+    }
 
     $.post("/tweets/", tweetText);
   });
